52e6a9cc9bd8bb65d7fd1d516ec765bba3e62733
mlt <miltozz@gmail.com>
2023-02-20 23:44:24 +0200

    upd env_vars.py and add sample .envs

--- python_tips/env_vars.py
+++ python_tips/env_vars.py
@@ -12,6 +12,10 @@ import os
 # read env vars
 print(os.getenv('EMAIL_ADDRESS'))
 print(os.getenv('EMAIL_PASSWORD'))
+print(os.getenv('DEPLOY'))
 
 print(os.environ.get('EMAIL_ADDRESS'))
 print(os.environ.get('EMAIL_PASSWORD'))
+print(os.environ.get('DEPLOY'))
+print(os.environ['DEPLOY'])
+print(os.environ['BACON'])
